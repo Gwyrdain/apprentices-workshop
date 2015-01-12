5505079b97ec5bb3a8ac65d941ada0916d11ccc5
Adam Bagby <ajbagby@gmail.com>
2015-01-12 01:45:59 +0000

    Added Bootstrap

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
+gem 'bootstrap-sass', '~> 3.3.1.0'
 
 group :development, :test do
     gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'home/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
