afa69bf39e22364f0f79ccd88f1b67ba34eca2d5
Adam Bagby <ajbagby@gmail.com>
2015-01-18 07:01:14 +0000

    added check for trap exit with down direction

--- app/models/exit.rb
+++ app/models/exit.rb
@@ -12,7 +12,7 @@ class Exit < ActiveRecord::Base
   validates :exittype, :numericality => { only_integer: true,
                                    greater_than_or_equal_to: -1,
                                    less_than_or_equal_to: 4,
-                                  }
+                                  },
   validates :keyvnum, :numericality => { only_integer: true,
                                    greater_than_or_equal_to: -1,
                                   }
@@ -20,13 +20,38 @@ class Exit < ActiveRecord::Base
                                    greater_than_or_equal_to: -1,
                                   }
   validates :name, length: { in: 4..75 }
+  
+
+  def dir_type_combo_ok?
+    if self.exittype == 3 # is a trap
+      if self.direction == 5 # is down
+        return true
+      else
+        return false
+      end
+    else
+      return true
+    end
+  end
+
 
   def destination_exists?
     if Room.exists?(:vnum => self.exitto) 
-       return true
+      return true
     else
       return false
     end
+  end
+  
+  def direction_word
+    $word = ''
+    $word = 'north' if self.direction == 0
+    $word = 'east' if self.direction == 1
+    $word = 'south' if self.direction == 2
+    $word = 'west' if self.direction == 3
+    $word = 'up' if self.direction == 4
+    $word = 'down' if self.direction == 5
+    return $word
   end
 
 end
