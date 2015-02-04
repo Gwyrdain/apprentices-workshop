91ea07fd450474c7c15e997c9bc814cb4096cce4
Adam Bagby <ajbagby@gmail.com>
2015-02-04 18:05:10 +0000

    added users index

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'home/about'
+  
+  get 'home/users'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
