ff9c9e8ea021587647ac225d9c9d275207de6325
Adam Bagby <ajbagby@gmail.com>
2016-03-29 04:30:38 +0000

    changed min length of extra desc from 4 to 2

--- app/models/oxdesc.rb
+++ app/models/oxdesc.rb
@@ -1,7 +1,7 @@
 class Oxdesc < ActiveRecord::Base
   belongs_to :obj
   
-  validates :keywords, length: { in: 4..75 }, format: { with: /\A[ -~]+\z/, message: "Only US-ASCII characters are permitted." }
+  validates :keywords, length: { in: 2..75 }, format: { with: /\A[ -~]+\z/, message: "Only US-ASCII characters are permitted." }
   validates :description, length: { minimum: 4 }, format: { with: /\A[\x0A\x0D -~]+\z/, message: "Only US-ASCII characters are permitted." }
 
 end
--- app/models/rxdesc.rb
+++ app/models/rxdesc.rb
@@ -1,7 +1,7 @@
 class Rxdesc < ActiveRecord::Base
   belongs_to :room
   
-  validates :keywords, length: { in: 4..75 }, format: { with: /\A[ -~]+\z/, message: "Only US-ASCII characters are permitted." }
+  validates :keywords, length: { in: 2..75 }, format: { with: /\A[ -~]+\z/, message: "Only US-ASCII characters are permitted." }
   validates :description, length: { minimum: 4 }, format: { with: /\A[\x0A\x0D -~]+\z/, message: "Only US-ASCII characters are permitted." }
 
 end
