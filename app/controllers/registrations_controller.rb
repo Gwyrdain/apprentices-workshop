0f4b86e5833a4115adbbd5cf90301a053bb08afc
Adam Bagby <ajbagby@gmail.com>
2016-04-19 04:43:39 +0000

    separated user profile from user settings

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def update    
+    @user = User.find(current_user.id)
+    successfully_updated = if needs_password?(@user, params)
+      @user.update_with_password(account_update_params)
+    else
+      params[:user].delete(:current_password)   
+      @user.update_without_password(account_update_params_no_password)
+    end
+  
+    if successfully_updated
+      set_flash_message :notice, :updated
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+
+
   private
 
   def sign_up_params
@@ -12,4 +31,13 @@ class RegistrationsController < Devise::RegistrationsController
                                   :password_confirmation, :current_password,
                                   :time_zone, :collapse_panels, :use_rulers)
   end
+
+  def account_update_params_no_password
+    params.require(:user).permit( :time_zone, :collapse_panels, :use_rulers)
+  end
+  
+  def needs_password?(user, params)
+    (params[:user].has_key?(:email) && user.email != params[:user][:email]) || !params[:user][:password].blank?
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :areas
-  
+
   include Bitfields
   
   bitfield :settings,
