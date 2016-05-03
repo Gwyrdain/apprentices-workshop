1d541481c986e8635c71eb3dddc0dc4fff41ef43
Adam Bagby <ajbagby@gmail.com>
2016-05-03 05:16:25 +0000

    added obj mob prefixes

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -10,6 +10,8 @@ module RoomsHelper
       output = output << "(Glowing) " if this_obj.glow?
       output = output << "(Humming) " if this_obj.hum?
       output = output << "(Invis) "   if this_obj.invis?
+      output = output << "(Blue Aura) " if this_obj.good?
+      output = output << "(Red Aura) "  if this_obj.evil?
 
       output = output << "#{this_obj.ldesc.gsub("<","&lt;").gsub(">","&gt;")}</font>"
       if safe
@@ -28,10 +30,12 @@ module RoomsHelper
       this_obj = Obj.find(id)
       output = "<font color=green>"
 
-      output = output << "(Magical) " if this_obj.magic?
-      output = output << "(Glowing) " if this_obj.glow?
-      output = output << "(Humming) " if this_obj.hum?
-      output = output << "(Invis) "   if this_obj.invis?
+      output = output << "(Magical) "   if this_obj.magic?
+      output = output << "(Glowing) "   if this_obj.glow?
+      output = output << "(Humming) "   if this_obj.hum?
+      output = output << "(Invis) "     if this_obj.invis?
+      output = output << "(Blue Aura) " if this_obj.good?
+      output = output << "(Red Aura) "  if this_obj.evil?
 
       output = output << "#{this_obj.sdesc.gsub("<","&lt;").gsub(">","&gt;")}</font>"
       return output.html_safe
@@ -46,11 +50,13 @@ module RoomsHelper
       this_mobile = Mobile.find(id)
       output = "<font color=white>"
 
-      output = output << "(Invis) "      if ( this_mobile.invisible? || this_mobile.improved_invis? )
-      output = output << "(Hide) "       if this_mobile.hide?
-      output = output << "(White Aura) " if this_mobile.sanctuary?
+      output = output << "(Invis) "       if ( this_mobile.invisible? || this_mobile.improved_invis? )
+      output = output << "(Hide) "        if this_mobile.hide?
+      output = output << "(White Aura) "  if this_mobile.sanctuary?
+      output = output << "(Pink Aura) "   if this_mobile.faerie_fire?
+      output = output << "(Translucent) " if this_mobile.passdoor?
       output = output << "(<font color=blue>Blue Aura</font>) " if this_mobile.alignment > 0
-      output = output << "(<font color=red>Red Aura</font>) "  if this_mobile.alignment < 0
+      output = output << "(<font color=red>Red Aura</font>) "   if this_mobile.alignment < 0
 
       output = output << "</font><font color=purple>"
       output = output << "#{this_mobile.ldesc.gsub("<","&lt;").gsub(">","&gt;")}</font>"
